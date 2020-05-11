7654466c5cf481cddbb53a58cd5025fa761544f3
luizborges <luizbborges@gmail.com>
2020-05-11 06:04:32 -0300

    upgrade Error class.

--- error/error.c
+++ error/error.c
@@ -36,42 +36,6 @@ static char ErrorMsg[4096] = "";
 ////////////////////////////////////////////////////////////////////////////////
 // Public Functions
 ////////////////////////////////////////////////////////////////////////////////
-void MError_(const char *file, const int line,
-             const char *func, ...)
-{
-  va_list msgUserArgs;
-  //////////////////////////////////////////////////////////////////////////////
-  // time information
-  //////////////////////////////////////////////////////////////////////////////
-  time_t timer = time(NULL);
-  const char *strTimer = asctime(localtime(&timer));
-  
-  //////////////////////////////////////////////////////////////////////////////
-  // file & line & function information
-  //////////////////////////////////////////////////////////////////////////////
-  // 1024 - is a size of string ErrorInfo - always check this size
-  // se a string resultante for maior que o size of string o restante que é maior será descartado
-  snprintf(ErrorInfo, 1024, "File: \"%s\"\nFunction: \"%s\"\nLine: %d", file, func, line);
-  
-  //////////////////////////////////////////////////////////////////////////////
-  // user msg information
-  //////////////////////////////////////////////////////////////////////////////
-  // se a string resultante for maior que o size of string o restante que é maior será descartado
-  va_start(msgUserArgs, func);
-  char *msgUser = va_arg(msgUserArgs, char*);
-  vsnprintf(ErrorMsg, 4096, msgUser, msgUserArgs); // size of ErrorMsg - 4096
-  va_end(msgUserArgs);
-  
-  //////////////////////////////////////////////////////////////////////////////
-  // user msg information
-  //////////////////////////////////////////////////////////////////////////////
-  // coloquei essa ERROR MSG em destaque, para melhor visualização no log do apache2
-  fprintf(stderr, "*****************************************\n");
-  fprintf(stderr, "**************  ERROR MSG  **************\n");
-  fprintf(stderr, "*****************************************\n");
-  fprintf(stderr, "ERROR %s\n%s\nTime Error: %s\n", ErrorMsg, ErrorInfo, strTimer);
-}
-
 void Error_(const char *file, const int line,
             const char *func, ...)
 {
@@ -110,9 +74,77 @@ void Error_(const char *file, const int line,
   abort();
 }
 
+void MError_(const char *file, const int line,
+             const char *func, ...)
+{
+  va_list msgUserArgs;
+  //////////////////////////////////////////////////////////////////////////////
+  // time information
+  //////////////////////////////////////////////////////////////////////////////
+  time_t timer = time(NULL);
+  const char *strTimer = asctime(localtime(&timer));
+  
+  //////////////////////////////////////////////////////////////////////////////
+  // file & line & function information
+  //////////////////////////////////////////////////////////////////////////////
+  // 1024 - is a size of string ErrorInfo - always check this size
+  // se a string resultante for maior que o size of string o restante que é maior será descartado
+  snprintf(ErrorInfo, 1024, "File: \"%s\"\nFunction: \"%s\"\nLine: %d", file, func, line);
+  
+  //////////////////////////////////////////////////////////////////////////////
+  // user msg information
+  //////////////////////////////////////////////////////////////////////////////
+  // se a string resultante for maior que o size of string o restante que é maior será descartado
+  va_start(msgUserArgs, func);
+  char *msgUser = va_arg(msgUserArgs, char*);
+  vsnprintf(ErrorMsg, 4096, msgUser, msgUserArgs); // size of ErrorMsg - 4096
+  va_end(msgUserArgs);
+  
+  //////////////////////////////////////////////////////////////////////////////
+  // user msg information
+  //////////////////////////////////////////////////////////////////////////////
+  // coloquei essa ERROR MSG em destaque, para melhor visualização no log do apache2
+  fprintf(stderr, "*****************************************\n");
+  fprintf(stderr, "**************  ERROR MSG  **************\n");
+  fprintf(stderr, "*****************************************\n");
+  fprintf(stderr, "ERROR %s\n%s\nTime Error: %s\n", ErrorMsg, ErrorInfo, strTimer);
+}
 
-
-
+void Warn_(const char *file, const int line,
+			const char *func, ...)
+{
+  va_list msgUserArgs;
+  //////////////////////////////////////////////////////////////////////////////
+  // time information
+  //////////////////////////////////////////////////////////////////////////////
+  time_t timer = time(NULL);
+  const char *strTimer = asctime(localtime(&timer));
+  
+  //////////////////////////////////////////////////////////////////////////////
+  // file & line & function information
+  //////////////////////////////////////////////////////////////////////////////
+  // 1024 - is a size of string ErrorInfo - always check this size
+  // se a string resultante for maior que o size of string o restante que é maior será descartado
+  snprintf(ErrorInfo, 1024, "File: \"%s\"\nFunction: \"%s\"\nLine: %d", file, func, line);
+  
+  //////////////////////////////////////////////////////////////////////////////
+  // user msg information
+  //////////////////////////////////////////////////////////////////////////////
+  // se a string resultante for maior que o size of string o restante que é maior será descartado
+  va_start(msgUserArgs, func);
+  char *msgUser = va_arg(msgUserArgs, char*);
+  vsnprintf(ErrorMsg, 4096, msgUser, msgUserArgs); // size of ErrorMsg - 4096
+  va_end(msgUserArgs);
+  
+  //////////////////////////////////////////////////////////////////////////////
+  // user msg information
+  //////////////////////////////////////////////////////////////////////////////
+  // coloquei essa ERROR MSG em destaque, para melhor visualização no log do apache2
+  fprintf(stderr, "*****************************************\n");
+  fprintf(stderr, "**************  WARNING MSG  ************\n");
+  fprintf(stderr, "*****************************************\n");
+  fprintf(stderr, "WARNING %s\n%s\nTime Warning: %s\n", ErrorMsg, ErrorInfo, strTimer);
+}
 
 
 
